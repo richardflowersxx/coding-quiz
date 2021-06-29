5bb4ab28efe924ac4d9ff5ad1c74696ff5f6f032
Ricardo Flores <richard.flowersxx@gmail.com>
2021-06-29 19:46:43 -0400

    updating index.html

--- script.js
+++ script.js
@@ -13,27 +13,26 @@ var questions = [
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        answer: 2
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        answer: 2
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        answer: 3
     },
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
-    },
+        answer: 2
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        answer: 3
     },
 
 ];
